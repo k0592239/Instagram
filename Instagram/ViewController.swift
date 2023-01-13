79616df9f848e06b9d61c7a937a4c128599d8a98
keiko.sato <120654447+k0592239@users.noreply.github.com>
2023-01-13 21:20:37 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 佐藤佳子 on 2023/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
