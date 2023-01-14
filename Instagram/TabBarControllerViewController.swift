5f9fdb0d8dad93070955475feb76677cf34b94ba
keiko.sato <120654447+k0592239@users.noreply.github.com>
2023-01-14 16:23:00 +0900

    〜Lesson 8 / Chapter 5.6：起動時にログインしていなければログイン画面を表示させる

--- Instagram/TabBarControllerViewController.swift
+++ Instagram/TabBarControllerViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class TabBarControllerViewController: UITabBarController, UITabBarControllerDelegate {
 
@@ -21,6 +22,15 @@ class TabBarControllerViewController: UITabBarController, UITabBarControllerDele
         self.tabBar.scrollEdgeAppearance = appearance
         // UITabBarControllerDelegateプロトコルのメソッドをこのクラスで処理する
         self.delegate = self
+    }
+    override func viewDidAppear(_ animated: Bool) {
+         super.viewDidAppear(animated)
+        // currentUserがnilならログインしていない
+        if Auth.auth().currentUser == nil {
+            // ログインしていないときの処理
+            let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+            self.present(loginViewController!, animated: true, completion: nil)
+        }
     }
     // タブバーのアイコンがタップされた時に呼ばれるdelegateメソッドを処理する
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
