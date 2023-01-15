a50a681d32af1b09c8bf2129639b80995b1784cf
keiko.sato <120654447+k0592239@users.noreply.github.com>
2023-01-15 21:49:50 +0900

    〜Lesson 8 / Chapter 9.2：投稿データを表示するカスタムTableViewCellを作成する

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 佐藤佳子 on 2023/01/15.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
